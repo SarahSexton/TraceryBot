0e45e9b3f4254785fb3d742c8efcd4fe8f56e146
Sarah Sexton <sarahjeannesexton@live.com>
2016-10-13 16:14:04 -0500

    Created package.json and index.html

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
 var tracery = require('tracery-grammar');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MY_APP_ID,
+    appPassword: process.env.MY_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
 
 var grammar = tracery.createGrammar({
   'animal': ['panda','fox','capybara','iguana'],
