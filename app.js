c4ac48d4a47286a253163a68c92fe65ceb721294
Sarah Sexton <sarahjeannesexton@live.com>
2016-10-13 22:57:21 -0500

    Added HTML code to make app mobile-friendly.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-// Add your requirements
+//Get Started with Microsoft Bot Framework in Node.js code at docs.botframework.com 
+//Add your requirements (as seen in package.json).
+//These dependencies are installed through Node.js command prompt with commands:
+//npm install --save tracery-grammar
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
 var tracery = require('tracery-grammar');
 
 // Setup Restify Server
+//"npm install --save restify" sets up a server to host your bot online.
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
@@ -16,6 +20,7 @@ server.get('/', restify.serveStatic({
 }));
   
 // Create chat bot
+//Generate your appId and appPassword by registering a bot: dev.botframework.com 
 var connector = new builder.ChatConnector({
     appId: process.env.MY_APP_ID,
     appPassword: process.env.MY_APP_PASSWORD
@@ -34,7 +39,7 @@ var grammar = tracery.createGrammar({
   'verb': 'administrate empower engage envision dominate leverage provide grow bring capitalize foster synergize unleash maximize'.split(' '),
   'adverb': 'organically completely efficiently dramatically professionally dynamically objectively collaboratively seamlessly'.split(' '),
   'adjective': 'agile robust iterative cutting-edge collaborative disruptive win-win cross-platform streamlined key self-sustaining extensible'.split(' '),  
-  'preposition': ['via','with','from','on','with','for','of','in','inside of','without','after','vis-a-vis','through','within'],
+  'preposition': ['via','with','from','on','with','for','of','in','inside of','without','after','vis-a-vis','through','within','out of'],
 //Expansion symbols can have modifiers. Modifiers can change something about the string expansion of that symbol.
   'origin':['#adverb.capitalize# #verb# #adjective# #noun# #preposition# #adjective# #noun#.'],
 });
