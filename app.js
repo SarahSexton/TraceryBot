7d161551844c1a3e8f392fe934ab30f1fb95a3bb
Sarah Sexton <sarahjeannesexton@live.com>
2016-10-13 20:57:04 -0500

    Added helpful comments and more word options.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
+//Direct to index.html web page
 server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html'
@@ -23,14 +24,18 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 //=========================================================
-// Tracery Grammar // I am #emotion.a# #animal#.
+// Tracery Grammar
 //=========================================================
+//A grammar is a key-value storage system for rules.
 var grammar = tracery.createGrammar({
-  'adjective': ['agile','robust','iterative','corporate','collaborative','overall','holistic','disruptive','win-win'],
-  'adverb': ['organically','completely','efficiently','dramatically','professionally','dynamically'],
-  'noun': ['growth strategies','scenarios','intellectual capital','frameworks','methodologies','technologies'],
-  'preposition': ['via','with','from','on','along','for','of','toward','in','inside of','without','after','vis-a-vis','through','within'],
-  'verb': ['administrate','empower','network with','engage','envision','dominate'],
+//Each symbol should be followed by an array of text strings representing rules
+  'noun': ['growth strategies','scenarios','takeaways','frameworks','methodologies','technologies','innovations','metrics','cloud solutions'],
+//or, if you're writing a long string of single words, you can use 'split'
+  'verb': 'administrate empower engage envision dominate leverage provide grow bring capitalize foster synergize unleash maximize'.split(' '),
+  'adverb': 'organically completely efficiently dramatically professionally dynamically objectively collaboratively seamlessly'.split(' '),
+  'adjective': 'agile robust iterative cutting-edge collaborative disruptive win-win cross-platform streamlined key self-sustaining extensible'.split(' '),  
+  'preposition': ['via','with','from','on','with','for','of','in','inside of','without','after','vis-a-vis','through','within'],
+//Expansion symbols can have modifiers. Modifiers can change something about the string expansion of that symbol.
   'origin':['#adverb.capitalize# #verb# #adjective# #noun# #preposition# #adjective# #noun#.'],
 });
 
@@ -39,7 +44,7 @@ grammar.addModifiers(tracery.baseEngModifiers);
 console.log(grammar.flatten('#origin#'));
 
 //=========================================================
-// Bots Dialogs
+// Bots Dialog
 //=========================================================
 
 bot.dialog('/', function (session) {
